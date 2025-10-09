f376dbfc2228b1858aa67856e925023cea8976de
kenji hasuo <baseballkenji300@gmail.com>
2025-10-09 16:04:18 +0900

    Todo::画面にデータの表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,7 +13,7 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index', ['helloWorld' => 'hello World!']);
+        return view('todo.index', ['todos' => $todos]);
     }
     //
 }
