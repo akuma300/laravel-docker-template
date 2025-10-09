76e9c7b782f82c2b75f32ff379d7a1997738cdcc
kenji hasuo <baseballkenji300@gmail.com>
2025-10-09 18:01:11 +0900

    Todo::リファクタリングの実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,8 +13,7 @@
 
 // 追加
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create'); // 追記
-
+Route::get('/todo/create', 'TodoController@create')->name('todo.create'); // 追記
 
 Route::get('/', function () {
     return view('welcome');
