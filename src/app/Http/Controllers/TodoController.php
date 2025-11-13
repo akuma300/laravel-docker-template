f80d255bff1ba5fb4bc2a6552a71e31eff96645f
kenji hasuo <baseballkenji300@gmail.com>
2025-11-13 18:13:17 +0900

    Todo::リファクタリングの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,10 +8,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo; // 追記
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo; // 追記
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -20,25 +26,20 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
-    
+
     public function store(Request $request) // 追記
     {
         $inputs = $request->all();
 
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->fill($inputs);
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
+        $todo = $this->todo->find($id);
 
         return view('todo.show', ['todo' => $todo]);
     }
